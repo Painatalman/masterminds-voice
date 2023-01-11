437e4746c70dd822f439b2733c58b8e2e45bdf60
JamieBurridge <xross_dev@proton.me>
2023-01-11 22:45:47 +0000

    Svelte typescript project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import './styles.css';
+	const testMessage: string = 'Masterminds voice';
+</script>
+
+<svelte:head>
+	<title>Masterminds Voice</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="main-section">
+	<h2>
+		{testMessage}
+	</h2>
+</section>
+
+<style>
+	.main-section {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		height: 100vh;
+	}
+</style>
